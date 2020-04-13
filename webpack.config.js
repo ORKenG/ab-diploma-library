897123e3a6d440706c3659a572ea262c16dc6370
oburia4 <oleh.buria@capgemini.com>
2020-04-13 18:46:28 +0300

    adjusted data collection

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,26 @@ const config = require('./package.json');
 const webpack = require('webpack');
 require('dotenv').config();
 
+/* eslint-disable no-undef*/
+
 const PROD = process.env.NODE_ENV === 'production';
 
 let plugins = [];
 
 PROD ? [
     plugins.push(new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
+        compress: { warnings: false }
     }))
-  ] : '';
+] : '';
 
 module.exports = {
-  entry: path.resolve(__dirname, config.main),
-  devtool: 'source-map',
-  output: {
-    library: process.env.NAME,
-    libraryTarget: process.env.TARGET,
-    path: __dirname,
-    filename: (PROD) ? 'build/ab-haze.min.js' : 'build/ab-haze.js'
-  },
-  plugins: plugins
+    entry: path.resolve(__dirname, config.main),
+    devtool: 'source-map',
+    output: {
+        library: process.env.NAME,
+        libraryTarget: process.env.TARGET,
+        path: __dirname,
+        filename: (PROD) ? 'build/ab-haze.min.js' : 'build/ab-haze.js'
+    },
+    plugins: plugins
 };
