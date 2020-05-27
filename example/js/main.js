dc3b834408468e8123be90ac15dac48852445518
oburia4 <oleh.buria@capgemini.com>
2020-05-27 17:49:52 +0300

    fixed carousel event

--- example/js/main.js
+++ example/js/main.js
@@ -13,52 +13,56 @@ abHaze.setAB('.ab-slider-image-1', '.ab-test-target', true);
 abHaze.setAB('.ab-slider-image-2', '.ab-test-target', true);
 
 $(window).on('load', function() {
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut();
-	$("#preloder").delay(400).fadeOut("slow");
+    /*------------------
+        Preloder
+    --------------------*/
+    $(".loader").fadeOut();
+    $("#preloder").delay(400).fadeOut("slow");
 
-	if($('.playlist-area').length > 0 ) {
-		var containerEl = document.querySelector('.playlist-area');
-		var mixer = mixitup(containerEl);
-	}
+    if($('.playlist-area').length > 0 ) {
+        var containerEl = document.querySelector('.playlist-area');
+        var mixer = mixitup(containerEl);
+    }
 
 });
 
 (function($) {
-	/*------------------
-		Navigation
-	--------------------*/
-	$(".main-menu").slicknav({
+    /*------------------
+        Navigation
+    --------------------*/
+    $(".main-menu").slicknav({
         appendTo: '.header-section',
-		allowParentLinks: true,
-		closedSymbol: '<i class="fa fa-angle-right"></i>',
-		openedSymbol: '<i class="fa fa-angle-down"></i>'
-	});
+        allowParentLinks: true,
+        closedSymbol: '<i class="fa fa-angle-right"></i>',
+        openedSymbol: '<i class="fa fa-angle-down"></i>'
+    });
 
-	$('.slicknav_nav').prepend('<li class="header-right-warp"></li>');
+    $('.slicknav_nav').prepend('<li class="header-right-warp"></li>');
     $('.header-right').clone().prependTo('.slicknav_nav > .header-right-warp');
 
-	/*------------------
-		Background Set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function() {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
 
 
-	$('.hero-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		mouseDrag: false,
-		animateOut: 'fadeOut',
-		animateIn: 'fadeIn',
-		items: 1,
-		autoplay: true
-	});
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        autoplay: true
+    });
+
+    $('.hero-slider').on('translated.owl.carousel', () => {
+        dispatchEvent(new Event('load'));
+    });
 
 })(jQuery);
 
